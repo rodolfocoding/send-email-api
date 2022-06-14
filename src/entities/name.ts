57d4c1cab65cbdba3c404403248849ac8ab06eea
rodolfocoding <rodolfo.coding@gmail.com>
2022-06-14 16:44:37 -0300

    feat: verify name while creating user

---
+++ src/entities/name.ts
@@ -0,0 +1,30 @@
+import { left, Either, right } from "@/shared/either";
+import { InvalidNameError } from "./errors/invalid-name-error";
+
+export class Name {
+  private readonly name: string;
+
+  private constructor(name: string) {
+    this.name = name;
+  }
+
+  static create(name: string): Either<InvalidNameError, Name> {
+    if (!Name.validate(name)) {
+      return left(new InvalidNameError());
+    }
+
+    return right(new Name(name));
+  }
+
+  static validate(name: string) {
+    if (!name) {
+      return false;
+    }
+
+    if (name.trim().length < 2 || name.trim().length > 256) {
+      return false;
+    }
+
+    return true;
+  }
+}
--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left } from "../shared/either";
 import { InvalidEmailError } from "./errors/invalid-email-error";
+import { InvalidNameError } from "./errors/invalid-name-error";
 import { User } from "./User";
 
 describe("User domain entity", () => {
@@ -8,5 +9,21 @@ describe("User domain entity", () => {
     const error = User.create({ name: "any_name", email: invalidEmail });
 
     expect(error).toEqual(left(new InvalidEmailError()));
+  });
+
+  it("should not create user with invalid name(too few characters)", () => {
+    const invalidName = "0   ";
+
+    const error = User.create({ name: invalidName, email: "any@mail.com" });
+
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  it("should not create user with invalid name(too many characters)", () => {
+    const invalidName = "N".repeat(257);
+
+    const error = User.create({ name: invalidName, email: "any@mail.com" });
+
+    expect(error).toEqual(left(new InvalidNameError()));
   });
 });
